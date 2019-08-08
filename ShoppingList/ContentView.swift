ea24c95e4ae27313095b4230af1235c62a3d3763
ericlewis <ericlewis777@gmail.com>
2019-08-08 13:15:55 -0400

    Initial Commit

---
+++ ShoppingList/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ShoppingList
+//
+//  Created by Eric Lewis on 8/8/19.
+//  Copyright © 2019 Eric Lewis, Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
